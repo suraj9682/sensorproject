133badcf14624bd73a98deac128d3d5f00431f74
Suraj Mishra <surajmishra9682@gmail.com>
2025-04-26 19:11:17 +0530

    testing workflow

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
         requirements.remove(HYPEN_E_DOT)
     return requirements
 
-
+# just update
 setup(
     name='Fault detection',
     version='0.0.1',
